babef99d729e671a8f9a00da008bdfeb727c43d5
KariukiAbel <abelmuriithi@gmail.com>
2020-03-21 10:12:54 +0300

    Finished the ValidateInput class

--- src/com/nabesh/ValidateInput.java
+++ src/com/nabesh/ValidateInput.java
@@ -10,5 +10,16 @@ public class ValidateInput {
         return lastName.matches("[a-zA-Z]+([ '-][a-zA-Z]+)*");
     }
 
+    public static boolean validateAddress(String address){
+        return address.matches("//d+//s+([a-zA-Z]+|[a-zA-Z]+\\s[a-zA-Z]+)");
+    }
+
+    public static boolean validateCity(String city){
+        return city.matches("([a-zA-Z]+|[a-zA-Z]+\\s[a-zA-Z]+)");
+    }
+
+    public static boolean validatePhoneNumber(String phone){
+        return phone.matches("\\d{3}-[1-9]\\d{3}-[0-9]\\d{3}-[0-9]\\d{3}-[0-9]");
+    }
 
 }

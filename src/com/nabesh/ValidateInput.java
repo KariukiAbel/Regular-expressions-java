e480250d91c06e01f97a73f4f07bedf30bc1d9bb
KariukiAbel <abelmuriithi@gmail.com>
2020-03-21 07:03:58 +0300

    Addded two fuctions to the class one checks for the validity of the first name and the other checks for validity of the last name

--- src/com/nabesh/ValidateInput.java
+++ src/com/nabesh/ValidateInput.java
@@ -2,4 +2,13 @@ package com.nabesh;
 
 public class ValidateInput {
 
+    public static boolean validaterFirstName(String firstName){
+        return firstName.matches("[A-Z][a-zA-Z]*");
+    }
+
+    public static boolean validateLastName(String lastName){
+        return lastName.matches("[a-zA-Z]+([ '-][a-zA-Z]+)*");
+    }
+
+
 }
